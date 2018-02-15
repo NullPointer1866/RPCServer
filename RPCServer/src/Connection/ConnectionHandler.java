3cb44707b7dff07047e85049021089cc941b7073
unknown <pwh001@lvc.edu>
2018-02-14 20:45:22 -0500

    Create pullMessage() to pull out entity as string

--- RPCServer/src/Connection/ConnectionHandler.java
+++ RPCServer/src/Connection/ConnectionHandler.java
@@ -1,11 +1,19 @@
 package Connection;
 
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Locale;
 
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.HttpException;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
+import org.apache.http.MethodNotSupportedException;
 import org.apache.http.nio.protocol.BasicAsyncRequestConsumer;
+import org.apache.http.nio.protocol.BasicAsyncResponseProducer;
 import org.apache.http.nio.protocol.HttpAsyncExchange;
 import org.apache.http.nio.protocol.HttpAsyncRequestConsumer;
 import org.apache.http.nio.protocol.HttpAsyncRequestHandler;
@@ -29,7 +37,43 @@ public class ConnectionHandler implements HttpAsyncRequestHandler<HttpRequest> {
 			throws HttpException, IOException {
 		
 		final HttpResponse response = httpExchange.getResponse();
+		handleInternal((HttpEntityEnclosingRequest) request, response, context);
+		httpExchange.submitResponse(new BasicAsyncResponseProducer(response));
+	}
+
+	private void handleInternal(HttpEntityEnclosingRequest request, HttpResponse response, HttpContext context) 
+			throws HttpException, IOException {
 		
+		// Check to make sure this is a POST request
+		// This "guarantees" that there will be an entity
+		String method = request.getRequestLine()
+				.getMethod()
+				.toUpperCase(Locale.ENGLISH);
+		
+		if(!method.equals("POST")) {
+			throw new MethodNotSupportedException(method 
+					+ " method not supported");
+		}
+		
+		HttpEntity body = request.getEntity();
+		
+		String jsonString = pullMessage(request, body);
+		
+	}
+
+	private String pullMessage(HttpEntityEnclosingRequest request, HttpEntity body)
+			throws UnsupportedOperationException, IOException {
+		
+		try(DataInputStream content = new DataInputStream(body.getContent())) {
+			
+			Header contentLength = request.getFirstHeader("Content-Length");
+			int len = Integer.parseInt(contentLength.getValue());
+			
+			byte[] raw_string = new byte[len];
+			content.readFully(raw_string, 0, len);
+			
+			return new String(raw_string);			
+		} 
 	}
 
 	@Override

58a537ba6d1daadfa9d04761bf591f9ef76dd1a8
Chloe McCarty <chloe_mccarty@hotmail.com>
2018-02-18 14:53:08 -0500

    refactored BL method interfaces

--- RPCServer/src/Parsing/RequestParser.java
+++ RPCServer/src/Parsing/RequestParser.java
@@ -1,6 +1,5 @@
 package Parsing;
 import java.util.Collection;
-import java.util.Dictionary;
 import java.lang.reflect.*;
 
 import com.google.gson.*;
@@ -24,23 +23,6 @@ public class RequestParser {
 		return response;
 	}
 
-/**
- * 
- * Handles the parsing of JSON responses 
- * either turning strings into JSON or 
- * vice-versa
- * 
- * build methods may be able to be replaced
- * by a single method if turning JSON into String
- * is trivial.
- *
- * If these methods are declared static, it may help
- * with asynchronous access. Static methods can be
- * declared "synchronized" and only one thread can
- * acess that method at a time, holds true for entire class 
- * type rather than instance.
- */
-	
 	
 	private String callMethod(String methodName) {
 		RPCResponse response;
@@ -58,6 +40,14 @@ public class RequestParser {
 		return g.toJson(response);
 	}
 
+	// determines name of method to call and calls method in business logic
+	public String parseMethodName() {
+		// get the method name
+		JsonObject jobj = new Gson().fromJson(json, JsonObject.class);
+		String methodName = jobj.get("methodName").getAsString();
+		return methodName;
+	}
+	
 	private RPCResponse purchaseItem() {
 		// turn json into an item, extract name
 		// wait this isn't what we want. We want to extract the parameters
@@ -66,12 +56,9 @@ public class RequestParser {
 		String itemName = params.get(0).getAsString();
 		int count = params.get(1).getAsInt();
 		Double price = BusinessLogic.purchaseItem(itemName, count);
-		String response;
+
 		
 		RPCResponse jsonResponse = new RPCResponse();
-		JsonArray arr = new JsonArray();
-		
-		
 		if (price < 0) {
 			jsonResponse.fault = "Error: not enough items in stock";
 			jsonResponse.status = 3; // precondition violation
@@ -92,8 +79,9 @@ public class RequestParser {
 		// get items, turn them into JSONarray
 		Collection<Item> coll = BusinessLogic.getItems(filter);
 		JsonArray arr = new JsonArray();
+		Type itemType = new TypeToken<Item>() {}.getType();
 		for(Item item : coll) {
-			JsonElement j = gson.toJsonTree(item);
+			JsonElement j = gson.toJsonTree(item, itemType);
 			arr.add(j);
 		}
 		
@@ -103,23 +91,4 @@ public class RequestParser {
 		return jsonResponse;
 	}
 	
-	// determines name of method to call and calls method in business logic
-    	/**
-	 * Parses JSON entity sent by client 
-	 * Determines what method they are calling and calls it
-	 * Should eventually return the JSON response back to the server
-	 * (Return chain comes out through this method)
-	 * 
-	 * @param jsonToParse - The string representation of a 
-	 * JSON object to parse
-	 * 
-	 * @return the JSON response to the method query 
-	 * formatted as a string for entity packing
-	 */
-	public String parseMethodName() {
-		// get the method name
-		JsonObject jobj = new Gson().fromJson(json, JsonObject.class);
-		String methodName = jobj.get("methodName").getAsString();
-		return methodName;
-	}
 }
--- RPCServer/src/Parsing/TestParsing.java
+++ RPCServer/src/Parsing/TestParsing.java
@@ -3,7 +3,7 @@ package Parsing;
 public class TestParsing {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
